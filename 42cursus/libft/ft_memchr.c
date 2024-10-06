995704c667e5285f368e919dec3a7f7c0cd6f231
Francisco Benjamin Rocca Cutillo <frocca-c@c2r4s1.42madrid.com>
2024-10-06 19:02:02 +0200

    Add ft_strtrim.c

--- 42cursus/libft/ft_memchr.c
+++ 42cursus/libft/ft_memchr.c
@@ -6,13 +6,13 @@
 /*   By: frocca-c <frocca-c@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/06 17:44:47 by frocca-c          #+#    #+#             */
-/*   Updated: 2024/10/06 17:44:47 by frocca-c         ###   ########.fr       */
+/*   Updated: 2024/10/06 17:48:13 by frocca-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	size_t	i;
+	unsigned int	i;
 
 	i = 0;
 	while (i < n)
@@ -21,5 +21,5 @@ void	*ft_memchr(const void *s, int c, size_t n)
 			return ((void *)s + i);
 		i++;
 	}
-	return (NULL);
+	return (0);
 }
--- 42cursus/libft/ft_strtrim.c
+++ 42cursus/libft/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: frocca-c <frocca-c@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/06 17:46:35 by frocca-c          #+#    #+#             */
-/*   Updated: 2024/10/06 17:46:36 by frocca-c         ###   ########.fr       */
+/*   Updated: 2024/10/06 19:00:40 by frocca-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,9 +25,59 @@ size_t	ft_strlen(const char *str)
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	unsigned int	len1;
-	char	*str;
+	char			*str1;
+	int				i;
+	int				j;
+	int				k;
+	int				m;
 
 	len1 = ft_strlen(s1);
-	str = (char *)malloc(len1 -);
-	while ()
+	str1 = (char *)malloc(len1);
+	i = 0;
+	k = 1;
+	while (s1[i] != '\0')
+	{
+		j = 0;
+		while (set[j] != '\0')
+		{
+			if (s1[i] == set[j])
+			{
+				i++;
+				k++;
+				break ;
+			}
+			j++;
+		}
+		i++;
+	}
+	m = 1;
+	while (len1 != -1)
+	{
+		j = 0;
+		while (set[j] != '\0')
+		{
+			if (s1[i - 1] == set[j])
+			{
+				i--;
+				m++;
+				break ;
+			}
+			j++;
+		}
+		len1--;
+	}
+	j = 0;
+	m = m - k;
+	while (s1[k] != '\0')
+	{
+		str1[j++] = s1[k++];
+	}
+}
+
+int main ()
+{
+	char const *s1 = "lolitalo";
+	char const *set = "lo";
+
+	ft_strtrim(s1, set);
 }
